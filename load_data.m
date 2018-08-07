34ec5d4e68a1882ad8f9ab277c2e4d4902e032a7
Daniellebenba <32228869+Daniellebenba@users.noreply.github.com>
2018-08-07 16:25:02 +0300

    change load_data
    added option to load subjects by order of subjects' number with excluding some specific subjects that we dont want to use their data

--- load_data.m
+++ load_data.m
@@ -20,6 +20,12 @@
 %Data's parameters:   
     %# of subjects:
     SUBJECTS = 2; 
+    
+    %Array of subjects' numbers that we won't load their files
+    notInclude = [2,3,4];
+    %Number of valid subjects:
+    valSUBJECTS = length(notInclude);
+    
     %Number of channels (signals): ECG + GSR:    
     CHANNELS = 2; 
     
@@ -41,7 +47,7 @@
     %(train_per of the subjects number rounded)
     bound = -1;
     if RANDOM_ALL == false
-        bound = round(SUBJECTS*train_per);
+        bound = round(valSUBJECTS*train_per);
     end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -58,7 +64,8 @@
     % <=> subject i is loaded correctly and the data in the block j is in use
     % 1<=j<=BLOCKS : block j in 'no Stress' condition
     % BLOCKS+1<=j<=BLOCKS*2 : block j-BLOCKS in 'Stress' condition
-
+    
+    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
@@ -72,107 +79,107 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Loop for all subjects
 for i = 1:SUBJECTS
+    if ismember(i,notInclude) == 0
+        %Subject's number:
+        sub_num = '00%d';
+        sub_num = sprintf(sub_num,i);
 
-    %Subject's number:
-    sub_num = '00%d';
-    sub_num = sprintf(sub_num,i+6);
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%Load ECG files: stress + noStress:
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %(***Need to add checking if the file wasn't loaded correctly****)
-    %(ST)
-    file_name = [PATH, sub_num, ECG_FILENAME_ST];
-    file_name = join(file_name);
-    ecg_st = load(file_name);
-    %(NS)
-    file_name = [PATH, sub_num, ECG_FILENAME_NS];
-    file_name = join(file_name);
-    ecg_ns = load(file_name);   
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %Filter the ECG data:
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %Remove Trends from Data
-    %(https://www.mathworks.com/help/signal/ug/remove-trends-from-data.html)
-    %%Eliminate linear trend
-    %(NS)
-    dt_ecgl = detrend(ecg_ns.EEG.data);
-    ecg_ns.EEG.data = dt_ecgl;
-    %(ST)
-    dt_ecg2 = detrend(ecg_st.EEG.data);
-    ecg_st.EEG.data = dt_ecg2;
-    %%Eliminate the nonlinear trend 
-    opol = 6;
-    %(NS)
-    t1 = (1:length(ecg_ns.EEG.data));
-    [p,s,mu] = polyfit(t1,ecg_ns.EEG.data,opol);
-    f_y1 = polyval(p,t1,[],mu);
-    dt_ecgnl = ecg_ns.EEG.data - f_y1;
-    ecg_ns.EEG.data = dt_ecgnl;
-    %(ST)
-    t2 = (1:length(ecg_st.EEG.data));
-    [p,s,mu] = polyfit(t2,ecg_st.EEG.data,opol);
-    f_y2 = polyval(p,t2,[],mu);
-    dt_ecgn2 = ecg_st.EEG.data - f_y2;
-    ecg_st.EEG.data = dt_ecgn2;
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%Parse all the blocks' times and parameters:
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %(More details explained in make_blocks() function)
-    blocks = make_blocks(ecg_ns, ecg_st); 
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%Load GSR files: stress + noStress:
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %(ST)
-    file_name = [PATH, sub_num, GSR_FILENAME_ST];
-    file_name = join(file_name);
-    gsr_st = load(file_name);
-    %(NS)
-    file_name = [PATH, sub_num, GSR_FILENAME_NS]; 
-    file_name = join(file_name);
-    gsr_ns = load(file_name);
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %Filter GSR data:
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    smoothdata(gsr_ns.data,'movmean',20); %**Maybe we want to change window size or add other filters
-    smoothdata(gsr_st.data,'movmean',20);
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %Down GSR's sampling rate:
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %Takes GSR data
-    gsr_ns_data = gsr_ns.data; 
-    gsr_st_data = gsr_st.data;
-    
-    %Down the sampling rate - From 1000 hz to 250 hz:
-    gsr_ns_down = downsample(gsr_ns_data,4);
-    gsr_st_down = downsample(gsr_st_data,4);
-    
-    %Add zeros to start for sync GSR with ECG data:
-    %Take starting time of the first block:
-    %(NS): 
-    start_t = blocks{1,1}(1,1);
-    gsr_ns_down = [zeros(1,start_t-1), gsr_ns_down];
-    %(ST): 
-    start_t = blocks{2,1}(1,1);
-    gsr_st_down = [zeros(1,start_t-1), gsr_st_down];
-    
-    %Takes GSR and ECG data and combine the two conditions per each
-    gsr_data{1,1} = gsr_ns_down;
-    gsr_data{2,1} = gsr_st_down;
-    ecg_data{1,1} = double(ecg_ns.EEG.data);
-    ecg_data{2,1} = double(ecg_st.EEG.data);
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %Make subject obj. and add to subjects array:
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    subjects(i) = subject(blocks, ecg_data, gsr_data);
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%Load ECG files: stress + noStress:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %(***Need to add checking if the file wasn't loaded correctly****)
+        %(ST)
+        file_name = [PATH, sub_num, ECG_FILENAME_ST];
+        file_name = join(file_name);
+        ecg_st = load(file_name);
+        %(NS)
+        file_name = [PATH, sub_num, ECG_FILENAME_NS];
+        file_name = join(file_name);
+        ecg_ns = load(file_name);   
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Filter the ECG data:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Remove Trends from Data
+        %(https://www.mathworks.com/help/signal/ug/remove-trends-from-data.html)
+        %%Eliminate linear trend
+        %(NS)
+        dt_ecgl = detrend(ecg_ns.EEG.data);
+        ecg_ns.EEG.data = dt_ecgl;
+        %(ST)
+        dt_ecg2 = detrend(ecg_st.EEG.data);
+        ecg_st.EEG.data = dt_ecg2;
+        %%Eliminate the nonlinear trend 
+        opol = 6;
+        %(NS)
+        t1 = (1:length(ecg_ns.EEG.data));
+        [p,s,mu] = polyfit(t1,ecg_ns.EEG.data,opol);
+        f_y1 = polyval(p,t1,[],mu);
+        dt_ecgnl = ecg_ns.EEG.data - f_y1;
+        ecg_ns.EEG.data = dt_ecgnl;
+        %(ST)
+        t2 = (1:length(ecg_st.EEG.data));
+        [p,s,mu] = polyfit(t2,ecg_st.EEG.data,opol);
+        f_y2 = polyval(p,t2,[],mu);
+        dt_ecgn2 = ecg_st.EEG.data - f_y2;
+        ecg_st.EEG.data = dt_ecgn2;
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%Parse all the blocks' times and parameters:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %(More details explained in make_blocks() function)
+        blocks = make_blocks(ecg_ns, ecg_st); 
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%Load GSR files: stress + noStress:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %(ST)
+        file_name = [PATH, sub_num, GSR_FILENAME_ST];
+        file_name = join(file_name);
+        gsr_st = load(file_name);
+        %(NS)
+        file_name = [PATH, sub_num, GSR_FILENAME_NS]; 
+        file_name = join(file_name);
+        gsr_ns = load(file_name);
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Filter GSR data:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        smoothdata(gsr_ns.data,'movmean',20); %**Maybe we want to change window size or add other filters
+        smoothdata(gsr_st.data,'movmean',20);
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Down GSR's sampling rate:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Takes GSR data
+        gsr_ns_data = gsr_ns.data; 
+        gsr_st_data = gsr_st.data;
+
+        %Down the sampling rate - From 1000 hz to 250 hz:
+        gsr_ns_down = downsample(gsr_ns_data,4);
+        gsr_st_down = downsample(gsr_st_data,4);
+
+        %Add zeros to start for sync GSR with ECG data:
+        %Take starting time of the first block:
+        %(NS): 
+        start_t = blocks{1,1}(1,1);
+        gsr_ns_down = [zeros(1,start_t-1), gsr_ns_down];
+        %(ST): 
+        start_t = blocks{2,1}(1,1);
+        gsr_st_down = [zeros(1,start_t-1), gsr_st_down];
+
+        %Takes GSR and ECG data and combine the two conditions per each
+        gsr_data{1,1} = gsr_ns_down;
+        gsr_data{2,1} = gsr_st_down;
+        ecg_data{1,1} = double(ecg_ns.EEG.data);
+        ecg_data{2,1} = double(ecg_st.EEG.data);
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %Make subject obj. and add to subjects array:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        subjects(i) = subject(blocks, ecg_data, gsr_data);
         
-    
+    end
 end 
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -197,7 +204,7 @@ end
         %Get cell array of the data split into segments and labels 
         %Add to 'data' and 'labels'
 
-    for i = 1:SUBJECTS
+    for i = 1:valSUBJECTS
         [tmp_data, tmp_labels, tmp_counter] = parser_levels(subjects(i), seg_len, over_lap);
 
         %Update data and labels
@@ -223,7 +230,7 @@ end
             counter = zeros(1,num_levels); %Reset the counter
             %**Need to reset data and labels also
         %Test data and labels:
-        elseif  i == SUBJECTS && RANDOM_ALL == false
+        elseif  i == valSUBJECTS && RANDOM_ALL == false
             [test_input, test_labels] = make_inputs_labels(data, labels, counter); 
         end
     end
