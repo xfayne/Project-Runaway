e9d85b17c6ac064ab27c72e3d6052bfb3d390e1a
Daniellebenba <32228869+Daniellebenba@users.noreply.github.com>
2018-08-28 20:03:05 +0300

    Update mainblack.py

--- mainblack.py
+++ mainblack.py
@@ -9,12 +9,16 @@ import scipy.io as sio
 import threading, subprocess
 from random import randint
 import sched, time as Time
+import matlab.engine
+import pickle
 
 #pg.setConfigOption('background', 'w')
 #pg.setConfigOption('foreground', 'k')
 
 count1 = 0
 FILEPATH = os.path.abspath(__file__)
+
+NETWORK = 0 #NETWORK == 1 <=> we use neural network, NETWORK == 0 <=> we usr classify
 
 ECGdata = []
 GSRdata = []
@@ -24,55 +28,52 @@ ECGtime = []
 GSRtime = []
 time = [GSRtime,ECGtime]
 
-segSize = 2200
-
-blocks = [(0,150),(160,250),(251,400)]
-
 results = [1,3,2,4,1,3]
 
-def predictSeg(GSRsignal,ECGsignal):
-    return
-    
-
-def predictBlock(GSRsignal,ECGsignal):
-    i = 0
-    prediction = []
-    while (i+segSize < len(signal)):
-        predict = predictSeg(signal[i:i+segSize])
-        predictions.extend(predict)
-        i+= segSize
-    return int(np.median(prediction))
-
-def makeBlocksList(tags, tags_time):
-    lst = []
-    for i in range(len(tags)):
-        if(tags[i] == 'start'): lst.append([tags_times[i],])
-        if(tags[i] == 'end'): lst[-1].append(tags_times[i])
-    return lst
-
-def clearSignal (signal):
-    clearedSignal = []
-    return clearedSignal
-    
-    
+filename = 'finalized Gradient Boosting Cllasifier.sav'
 
 ########## methods for real time grahps plotting ############
+# Run .mat scripts
+eng = matlab.engine.start_matlab()  ##Need to bring up
 
-def updateGSRplot(starttime):
+def predictSeg(ECGsignal, GSRsignal):
+
+    if NETWORK == 1:
+        input_seg = eng.mat_signals(ECGsignal, GSRsignal)  # Run .mat script to filter signals
+        X = input_seg
+        return input_seg
+    else:
+        input_features = eng.mat_features(ECGsignal, GSRsignal)  # Run .mat script to extruct features from signals
+
+        # load the model from disk
+        loaded_model = pickle.load(open(filename, 'rb'))
+        # result = loaded_model.score(X_test, Y_test)
+
+        # predict the result of the segment
+        result = loaded_model.predict(input_features)
+        print(result)
+        return result
+
+
+
+def updateGSRplot(starttime, update_time):
+    update_time = 0.2
+    buf_read = 200
     # updates the raw GSR plot
     # getting paramater 'starttime' = time of the clock when this method had been called
-    # every 0.2 sec the plot is being updating with more 200 data value
-    global GSRdata, GSRtime,GSRcurve,count1, RTdataGSR, RTtimeGSR
+    # every update_time= 0.2 sec the plot is being updating with more 200 data value
+    global GSRdata, GSRtime,GSRcurve,count1
     RTdata = []
     RTtime = []
     while True:
-        RTdataGSR.extend(GSRdata[count1:(count1+200)])
-        RTtimeGSR.extend(GSRtime[count1:(count1+200)])
-        count1+=200
-        print(RTdataGSR[count1-1])
-        GSRcurve.setData(RTtimeGSR,RTdataGSR)
+        #The simulation: reading the data in real time
+        RTdata.extend(GSRdata[count1:(count1+buf_read)])
+        RTtime.extend(GSRtime[count1:(count1+buf_read)])
+        count1+=buf_read
+        print(RTdata[count1-1]) #Printing the data in the kernel
+        GSRcurve.setData(RTtime,RTdata) #Updates the gsr plot
         QtGui.QApplication.processEvents()
-        Time.sleep(0.2 - ((Time.time() - starttime) % 0.2)) #Time.sleep(x) hold the system for x time
+        Time.sleep(update_time - ((Time.time() - starttime) % update_time)) #Time.sleep(x) hold the system for x time
 
 '''def updateGSRplot(starttime):
     # updates the raw GSR plot
@@ -92,31 +93,23 @@ def updateECGplot(starttime,sync_time):
     # getting paramater 'starttime' = time of the clock when this method had been called
     # and aramater 'sync_time' = time of the GSR when this method had been called
     # every 0.2 sec the plot is being updating with more 200 data value
-    global ECGdata ,ECGtime, ECGcurve, RTdataECG, RTtimeECG
-    count2 = count1 # = sync_time 
+    global ECGdata ,ECGtime, ECGcurve
+    count2 = sync_time
     while True:
-        RTdataECG.extend(ECGdata[count2:(count2+200)])
-        RTtimeECG.extend(ECGtime[count2:(count2+200)])
-        count2+=200
-        print(RTdataECG[count2-1])
-        ECGcurve.setData(RTtimeECG,RTdataECG)
-        QtGui.QApplication.processEvents()
-        Time.sleep(0.2 - ((Time.time() - starttime) % 0.2)) #Time.sleep(x) hold the system for x time
-
-        '''
         count2+=200
         print(ECGdata[count2])
         ECGcurve.setData(ECGtime[sync_time:count2],ECGdata[sync_time:count2])
         QtGui.QApplication.processEvents()
-        Time.sleep(0.2 - ((Time.time() - starttime) % 0.2))'''
+        Time.sleep(0.2 - ((Time.time() - starttime) % 0.2))
 
     
 ############## methods for gui interfence ###################
 
+#Main for GUI
 class runBlack (QDialog):
     def __init__(self):
         super(runBlack,self).__init__()
-        loadUi('runBlack.ui',self)
+        loadUi('runBlack.ui',self) #User interface
         self.setWindowTitle('hello world')
         self.loadGSR.clicked.connect(self.loadGSR_clicked)
         self.loadECG.clicked.connect(self.loadECG_clicked)
@@ -138,51 +131,58 @@ class runBlack (QDialog):
         for t in tmp: ECGtime += [t/1000]
         ECGdata = m['data'][0]
         del m'''
-        ECGtime = GSRtime
-        ECGdata = np.random.normal(loc=6.5, scale=0.3, size=len(GSRtime))
+        m = sio.loadmat(fecg)
+        ECGtime = m['times'][0]
+        ECGdata = m['data'][0]
+
+        #ECGdata = np.random.normal(loc=6.5, scale=0.3, size=len(GSRtime)) #simulates graph
         data = [GSRdata,ECGdata]
         time = [GSRtime,ECGtime]
         
         if self.rtMode.isChecked() == True:
         # load ECG clicked when the system is on real time mode #
             sync_time = count1 # = GSR current position
-            start_time=Time.time()
+            start_time=Time.time() #Current time
             threading.Timer(0, updateECGplot, (start_time,sync_time,)).start() #start ploting ECG on new thread
             
         else:
         # load ECG clicked when the system is on static test mode #
             ECGcurve.setData(ECGtime,ECGdata)
 
-    
+    #GSR#####################
     def loadGSR_clicked(self):
         
         global ECGdata, data, GSRdata, ECGtime, GSRtime, time, GSRcurve, ECGcurve, p
         
-        if GSRdata ==[]: 
+        if GSRdata == []: #If empty
+            #Initialize plot
             p = self.rawDataView.addPlot()
             ECGcurve = p.plot(pen=(1,2))
             GSRcurve = p.plot(pen=(0,2))
-        
+
+        #Load only .mat file
         fgsr = QFileDialog.getOpenFileName(self, 'Open file',"mat files (.mat)")[0]
-        m= sio.loadmat(fgsr)
+        m = sio.loadmat(fgsr)
         GSRtime = m['time_index'][0]
         GSRdata = m['data'][0]
-        del m
+        del m #We don't need the matrix anymomre
         data = [GSRdata,ECGdata]
         time = [GSRtime,ECGtime]
+
+        #Positions in window
         p.setXRange(-1.06, 878.15)
         p.setYRange(4.82, 9.3)
         
         if self.rtMode.isChecked() == True:
         # load ECG clicked when the system is on real time mode #
-            starttime=Time.time()
+            starttime=Time.time() #Current time
             threading.Timer(0, updateGSRplot, (starttime,)).start() #start ploting GSR on new thread
 
         else:
         # load GSR clicked when the system is on static test mode #
             GSRcurve.setData(GSRtime,GSRdata)
 
-            
+
                 
     def start_clicked(self):
         
@@ -194,10 +194,6 @@ class runBlack (QDialog):
             # system is on real time test mode #
                 data = [1,3,5,2,6,2]
                 self.rawDataView.plot([0,1,2,3,4,5],data,pen=(0,2))
-                starttime=Time.time()
-                threading.Timer(0, updateGSRplotRT, (starttime,)).start() #start ploting GSR on new thread
-                starttime=Time.time()
-                threading.Timer(0, updateECGplotRT, (starttime,)).start() #start ploting GSR on new thread
             
             else:
             # system is on static test mode #
@@ -205,21 +201,25 @@ class runBlack (QDialog):
                 p1 = self.modDataView.addPlot(x=time[1], y=data[1], pen=(1,2))
                 p2 = pg.PlotCurveItem(x=time[0], y=data[0], pen=(0,2))
                 p1.addItem(p2)
-                
-                for i in range (len(blocks)):
-                # add the network workeloads predictons of each data block to the graphic view
-                    level = predictBlock(GSRdata[blocks[i][0]:blocks[i][1]],ECGdata[blocks[i][0]:blocks[i][1]])
-                    lr = pg.LinearRegionItem(values=[150*(i), 150*(i+1)], brush=pg.intColor(index=level,alpha=50), movable=False)
-                    p1.addItem(lr)
-                    label = pg.InfLineLabel(lr.lines[1], "oveload "+str(level) , position=0.85, rotateAxis=(1,0), anchor=(1, 1))
-                
-               ''' for i in range (len(results)):
+
+
+                #Add classification
+
+
+
+
+
+
+
+
+                #After we predict all the blocks levels
+                for i in range (len(results)):
                 # add the network workeloads predictons of each data block to the graphic view
                     level = results[i]
                     lr = pg.LinearRegionItem(values=[150*(i), 150*(i+1)], brush=pg.intColor(index=level,alpha=50), movable=False)
                     p1.addItem(lr)
                     label = pg.InfLineLabel(lr.lines[1], "oveload "+str(level) , position=0.85, rotateAxis=(1,0), anchor=(1, 1))
-'''
+
         else:
         # either GSR or ECG files has not been loaded
             print("error")
